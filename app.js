8af8d792c897528ce8e0085143468651c1b5bb6b
nathan-potts41 <pottsandpans41@hotmail.com>
2020-11-23 18:01:42 -0600

    moved generatePage to its own file

--- app.js
+++ app.js
@@ -1,42 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
 const fs = require('fs');
-// // Notice the lack of parentheses around the `profileDataArr` parameter?
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//       console.log(profileDataArr[i]);
-//     }
 
-//     console.log('================');
+const generatePage = require('./src/page-template.js');
 
-//     // Is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-//   };
+const profileDataArgs = process.argv.slice(2);
 
-// printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
 
-const generatePage = (userName, githubName) => {
-    return `
-    <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-    `;
-};
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw (err);
-
-    console.log("Portfolio complete! Checkout index.html to see the output!");
+  console.log('Portfolio complete! Check out index.html to see the output!');
 });
